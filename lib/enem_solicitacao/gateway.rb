627ea8291748cfd4f9d5cc7c8c4642ddeaabafe5
Diego Aguir Selzlein <diegoselzlein@gmail.com>
2015-01-27 08:01:43 -0200

    Permitindo busca por CPF.

--- lib/enem_solicitacao/gateway.rb
+++ lib/enem_solicitacao/gateway.rb
@@ -24,6 +24,26 @@ module EnemSolicitacao
       last_result
     end
 
+    def search_by_cpf(cpf, year = EnemSolicitacao.year)
+      page = agent.get(EnemSolicitacao.path('/solicitacao/'\
+        "resultado#{year}/cpf/solicitacaoPelaInternet.seam"))
+      form = page.form_with(id: 'formularioForm')
+      form.enctype = 'application/x-www-form-urlencoded'
+      form['cpfDecorate:cpfInput'] = cpf
+      form['j_id131.x'] = 81
+      form['j_id131.y'] = 23
+
+      page = form.submit
+      form = page.form_with(id: 'resultadoForm')
+      form.enctype = 'application/x-www-form-urlencoded'
+      form['j_id191.x'] = 72
+      form['j_id191.y'] = 19
+      page = form.submit
+
+      fail 'Request problem' unless page.body['sucesso']
+      last_result
+    end
+
     def last_result
       page = agent.get(EnemSolicitacao.path('/solicitacao/acompanhar'\
         'Solicitacao.seam'))
--- test/enem_solicitacao/gateway_test.rb
+++ test/enem_solicitacao/gateway_test.rb
@@ -14,4 +14,14 @@ RESPONSE
 
     VCR.eject_cassette
   end
+
+  def test_gets_candidate_info_by_cpf
+    VCR.insert_cassette 'test_gets_candidate_info_by_cpf'
+
+    assert_equal gateway.search_by_cpf('11111111111'), <<RESPONSE.strip
+123456789012;11111111111;AAAAAAAAA BBBBBBBBB CCCCCCCCC;610.9;639.5;596.4;580.4;700.0;7;7;7;7;7;01/01/1990;M;2222222;SSP;SC;PR;FOZ DO IGUACU;N;Espanhol;
+RESPONSE
+
+    VCR.eject_cassette
+  end
 end
