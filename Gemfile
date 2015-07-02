fcf851badc5de962bcc82cfafe4c82a6045f3b44
Diego Aguir Selzlein <diego.selzlein@unila.edu.br>
2015-07-02 10:46:00 -0300

    Configuração para Code Climate.

--- Gemfile
+++ Gemfile
@@ -7,3 +7,4 @@ gem 'webmock', '~> 1.18'
 gem 'dotenv', '~> 1.0'
 gem 'rake', '~> 10.4'
 gem 'minitest', '~> 5.7'
+gem 'codeclimate-test-reporter', group: :test, require: nil
--- test/support/vcr.rb
+++ test/support/vcr.rb
@@ -1,4 +1,6 @@
 VCR.configure do |v|
+  v.ignore_hosts 'codeclimate.com'
+
   path = File.expand_path("../../", __FILE__)
   v.cassette_library_dir = "#{path}/vcr"
   v.hook_into :webmock
